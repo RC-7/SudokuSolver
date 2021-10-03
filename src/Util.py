4c815e124a16fc6b4c2fe01fef5d0574f32fc201
Rashaad Cassim <rashcassim@gmail.com>
2021-10-03 15:18:58 +0200

    Adding basic layout to Util class/file
    Adding some basic member variables that will be used and the ability to
    read an image

--- src/Util.py
+++ src/Util.py
@@ -1,4 +1,6 @@
 import json
+import cv2
+import numpy as np
 
 
 def read_label(puzzleNumber):
@@ -7,8 +9,45 @@ def read_label(puzzleNumber):
     labels = json.load(label_file)['puzzleLabel']
     return labels
 
+def view_image(image):
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 class Util:
+    def __init__(self, debug=False):
+        self.contoursOriginal = []
+        self.cells = []
+        self.contoursBoard = []
+        self.original = []
+        self.board = []
+        self.labels = []
+        self.debug = debug
+        self.puzzle_name = "Puzzle1"
 
-    def __init__(self):
+    def set_puzzle(self, puzzleNumber):
+        self.puzzle_name = "Puzzle" + puzzleNumber
+
+    def read_label(self):
+        filename = "../data/Labels/" + self.puzzle_name + ".json"
+        label_file = open(filename)
+        labels = json.load(label_file)['puzzleLabel']
+        self.labels = labels
+
+    def read_image(self):
+        filename = "../data/Images/" + self.puzzle_name + ".jpg"
+        original = cv2.imread(filename)
+        self.original = cv2.resize(original, (700, 960))
+        if self.debug:
+            view_image(self.original)
+
+    def process_image(self):
         pass
+
+    def get_cells(self):
+        pass
+
+    def get_board(self):
+        pass
+
--- src/main.py
+++ src/main.py
@@ -2,3 +2,4 @@ from src.Util import Util
 
 util = Util()
 
+util.read_image()
