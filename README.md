447ed1ae78bc6543d61f076774d0ce1f7a6465b8
Rashaad Cassim <rashcassim@gmail.com>
2021-10-10 19:47:55 +0200

    Minor edits
    Minor change to README to track TODO's and a needed update to main.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SudokuSolver
 A Sudoku solver, solving and annotating images of Sudoku puzzles.
+
+### TODO
+- [ ] Add UI wrapper
+- [ ] Add ability to ask for hint instead of solve the whole Puzzle
+- [ ] Add ability to solve puzzles when no one cell has a definite next step 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
-
 from src.SudokuPuzzle import SudokuPuzzle
-sp = SudokuPuzzle('ZoomTest', False)
-sp.create_board()
-sp.solve_puzzle()
+
+
+def main():
+    sp = SudokuPuzzle('ZoomTest', False)
+    sp.create_board()
+    sp.solve_puzzle()
+
+
+if __name__ == "__main__":
+    main()
